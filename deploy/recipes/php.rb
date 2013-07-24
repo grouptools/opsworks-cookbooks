96c783a414c98e067a7ef55c157f99f2327e07f6
grouptools <fabioaguiar@grouptools.com.br>
2013-07-24 13:19:26 -0300

    Deploy user

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -13,8 +13,8 @@ node[:deploy].each do |application, deploy|
   end
 
   opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
+    user 'apache' #deploy[:user]
+    group 'apache' #deploy[:group]
     path deploy[:deploy_to]
   end
 
